035c5c9a6d3a667db14e76f4423cacbdce5645b3
Bryan O'Neill <bro9918@g.rit.edu>
2014-09-11 20:46:43 -0400

    Feedback progress

---
+++ Assets/Scripts/Feedback.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Feedback : MonoBehaviour {
+
+	public GameObject pSysPrefab;
+	private GameObject player;
+	private GameObject pSys;
+	private Color currentColor;
+
+	// Use this for initialization
+	void Start () {
+		pSys = (GameObject)Instantiate(pSysPrefab);
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pSys.transform.parent = player.transform;
+		pSys.transform.localPosition = new Vector3(0, 0, 0);
+		currentColor = player.renderer.material.color;
+		pSys.particleSystem.startColor = currentColor;
+	}
+}
