af0e57bd330d8cfbe7ccbe234411e2751d370df1
Robert  Lumsden <rml8300@rit.edu>
2014-09-12 15:07:51 -0400

    Created Original Player Prefab, FirstTracer, FirstMover Scripts for the original player behaviour

---
+++ Assets/Scripts/FirstTracer.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstTracer : MonoBehaviour {
+	public Camera gameCamera = null;
+	private LineRenderer lineRenderer = null;
+	public FirstMover mover;
+	private int vertexCount = 0;
+	private int stableVertexCount = 0;
+	public float minDragToDraw = 1.0f;
+	public GameObject lineMakerPrefab = null;
+	private Vector3 almostLastVertex = Vector3.zero;
+	private Vector3 lastVertex = Vector3.zero;
+	private Vector3 lastDirection = Vector3.zero;
+	private Vector3 lastStableVertex = Vector3.zero;
+	private Vector3 lastStableDirection = Vector3.zero;
+	
+	void Start() {
+		/*GameObject globalsObject = GameObject.FindGameObjectWithTag("Globals");
+		if (globalsObject == null) {
+			Application.LoadLevel("Main screen");	
+		}
+		else {
+			Globals globals = globalsObject.GetComponent<Globals>();
+			critMat = (Material)Instantiate(Resources.Load(globals.critmat));
+			critLineMakerPrefab = (GameObject)Instantiate(Resources.Load(globals.critprefab));
+			nonCritMat = (Material)Instantiate(Resources.Load(globals.noncritmat));
+			nonCritLineMakerPrefab = (GameObject)Instantiate(Resources.Load(globals.noncritprefab));
+		}*/
+		
+		mover = GetComponent<FirstMover>();
+		mover.moving = false;
+		vertexCount = 0;
+		stableVertexCount = 0;
+	}
+	
+	void Update() {
+		HandleTouches();
+	}
+	
+	private void HandleTouches() {		
+		if (Input.GetMouseButtonDown (0)) {
+			StartLine();
+		} else if (Input.GetMouseButton (0)) {
+			DragAndDraw();
+		}
+	}
+	
+	public void StartLine(bool startAtVertex = false, Vector3 startVertex = new Vector3())
+	{		
+		//Vector3 mousePosition = MousePointInWorld();
+		//transform.position = MousePosition;
+		
+		// If this line is not on the required path, create a separate polyline.
+		CreateLineMaker(true);
+		
+		AddVertex(transform.position);
+	}
+	
+	private void DragAndDraw(bool criticalLine = true) {
+		Vector3 mousePosition = MousePointInWorld();
+		Vector3 toMouse = mousePosition - transform.position;
+		if (toMouse.sqrMagnitude > minDragToDraw * minDragToDraw)
+		{
+			//transform.Translate (translationToTouch);
+			float toMouseMag = toMouse.magnitude;
+			if (toMouseMag > 0)
+			{
+				toMouse /= toMouseMag;
+				float speedRamp = 1;
+				/*if (speedRampRadius > 0)
+				{
+					speedRamp = toMouseMag / speedRampRadius;
+				}*/
+				float moveDist = mover.maxSpeed * speedRamp;
+				if (moveDist > toMouseMag)
+				{
+					moveDist = toMouseMag;
+				}
+				mover.Move(toMouse, moveDist, true);
+			}
+			mover.moving = true;
+			AddVertex(transform.position);
+		}
+	}
+	
+	private Vector3 MousePointInWorld() {
+		Vector3 touchPosition = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+		touchPosition.z = transform.position.z;
+		return touchPosition;
+	}
+	
+	private void AddVertex(Vector3 position) {	
+		if (vertexCount > 1) {
+			// Preserve look of the most recent line segement if the new vertex
+			// drastically changes the direction of motion. Without this, the line segement
+			// gets distorted as only the center point is ever stored.
+			if (Vector3.Dot((position - lastVertex).normalized, lastDirection) < 0.6f) {
+				lineRenderer.SetVertexCount(++vertexCount);
+				Vector3 midPosition = lastVertex + (lastDirection * minDragToDraw / 4);
+				lineRenderer.SetPosition(vertexCount - 1, midPosition); 
+				lastVertex = midPosition;
+			}
+		}
+		
+		lineRenderer.SetVertexCount(++vertexCount);
+		lineRenderer.SetPosition(vertexCount - 1, position); 
+		lastDirection = (position - lastVertex).normalized;
+		almostLastVertex = lastVertex;
+		lastVertex = position;
+	}	
+	
+	public void CreateLineMaker(bool criticalLine) {		
+		GameObject newLineMaker = (GameObject)Instantiate(lineMakerPrefab, Vector3.zero, Quaternion.identity);
+		newLineMaker.transform.parent = transform;
+		lineRenderer = newLineMaker.GetComponent<LineRenderer>();
+		lineRenderer.SetVertexCount(0);
+		vertexCount = 0;
+	}
+}
---
+++ Assets/Scripts/Points/DeepDetail.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeepDetail : MonoBehaviour {
+
+public Material myMaterial;
+	
+public bool isHit = false;
+private bool isHitOnce = false;
+private bool waiting = false;
+	
+// Use this for initialization
+void Start () {
+		
+	}
+	
+// Update is called once per frame
+void Update () {
+
+	}
+	
+	void OnTriggerEnter (Collider collide)
+	{
+		if (collide.gameObject.tag == "Player")
+		{
+			if(isHitOnce == false && isHit == false)
+			{
+				setHitOnce();
+				waiting = true;
+				Invoke("setWaiting", 1.0f);
+			}
+
+			if(isHitOnce == true && waiting == false)
+				setHitOn();
+
+			if(isHit == true)
+				setHitOn();
+		}
+		
+	}
+	
+	
+	void setHitOnce ()
+	{
+		isHitOnce = true;
+		renderer.material.color = Color.red; 
+		Invoke("setHitOnceOff",5.0f);
+	}
+	
+	void setHitOnceOff ()
+	{
+		isHitOnce = false;
+		renderer.material = myMaterial;  
+	}
+	
+	void setHitOn ()
+	{
+		isHit = true;
+		renderer.material.color = Color.blue;
+		Invoke("setHitOff",5.0f);
+	}
+	
+	void setHitOff ()
+	{
+		isHit = false;
+		setHitOnce();
+		renderer.material.color = Color.red;
+	}
+
+	void setWaiting ()
+	{
+		waiting = false;
+	}
+	
+	
+	
+}
