22e0ae73a1dc6cf44cfbb0ee48cb921512c3e342
Bryan O'Neill <bro9918@g.rit.edu>
2014-09-12 21:16:32 -0400

    particles and sound effect

--- Assets/Scripts/Feedback.cs
+++ Assets/Scripts/Feedback.cs
@@ -4,21 +4,54 @@ using System.Collections;
 public class Feedback : MonoBehaviour {
 
 	public GameObject pSysPrefab;
+	public GameObject colorExplosionPrefab;
 	private GameObject player;
 	private GameObject pSys;
+	private GameObject colExp;
 	private Color currentColor;
+	private Vector3 prevPos;
+	private Vector3 currentDir;
+	private Color boostColorOne;
+	private Color boostColorTwo;
+	private Color boostColorThree;
+	private Color boostColorFour;
+	private bool exploded = false;
 
 	// Use this for initialization
 	void Start () {
 		pSys = (GameObject)Instantiate(pSysPrefab);
 		player = GameObject.FindGameObjectWithTag("Player");
+		prevPos = player.transform.position;
+		boostColorOne = new Color(0.3f, 0.5f, 0.3f, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		pSys.transform.parent = player.transform;
-		pSys.transform.localPosition = new Vector3(0, 0, 0);
+		pSys.transform.position = player.transform.position;
 		currentColor = player.renderer.material.color;
 		pSys.particleSystem.startColor = currentColor;
+
+
+
+		currentDir = player.transform.position - prevPos;
+
+		pSys.particleSystem.emissionRate = currentDir.magnitude/Time.deltaTime;
+
+		currentDir.Normalize();
+
+
+		if(currentDir.sqrMagnitude != 0)
+			pSys.transform.rotation = Quaternion.LookRotation(-currentDir, pSys.transform.up);
+		prevPos = player.transform.position;
+
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			player.renderer.material.color = boostColorOne;
+			colExp = (GameObject)Instantiate(colorExplosionPrefab);
+			colExp.particleSystem.startColor = player.renderer.material.color;
+			exploded = true;
+		}
+		if(exploded == true)
+			colExp.transform.position = player.transform.position;
 	}
 }
