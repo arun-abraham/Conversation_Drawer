3835a60a610c214c3487026f8e07468b0e30f4c2
Arun Abraham <aa6096@g.rit.edu>
2014-09-13 13:28:07 -0400

    Hooked up XInput. Working on vibration settings.

---
+++ Assets/Scripts/ControllerFeedback.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class ControllerFeedback : MonoBehaviour {
+
+	float duration;
+	float intensity;
+	float startTime;
+
+	// Use this for initialization
+	void Start () {
+		intensity = 0.0f;
+		duration = 3.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - startTime < duration)
+		{
+			GamePad.SetVibration(0, intensity, intensity);
+		}
+	}
+
+	void HardVibrate (float time) {
+		startTime = Time.time;
+		intensity = 0.5f;
+		duration = 0.5f;
+	}
+}
