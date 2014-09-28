db4d2d524777f8d7e6b99ea2b26d1caba2e726cc
Bryan O'Neill <bro9918@g.rit.edu>
2014-09-28 14:00:14 -0400

    Added beep sound and slow player script

---
+++ Assets/Scripts/SlowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowPlayer : MonoBehaviour {
+
+	private bool slowing = false;
+	private SimpleMover mover;
+	private WaypointSeek seeker;
+	private PartnerLink partner;
+
+	// Use this for initialization
+	void Start () {
+		partner = GetComponent<PartnerLink>();
+		mover = partner.Partner.GetComponent<SimpleMover>();
+		seeker = GetComponent<WaypointSeek>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(seeker.orbit == true && slowing == false)
+		{
+			slowing = true;
+			mover.externalSpeedMultiplier -= .2f;
+		}
+		else if(slowing == true)
+		{
+			slowing = false;
+			mover.externalSpeedMultiplier += .2f;
+		}
+	}
+}
